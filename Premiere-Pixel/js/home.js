68889e2795c503a67b9b42dd5403f0f854f116a5
box-one-zero <warren22palabay24@gmail.com>
2025-06-04 08:12:43 +0800

    Update home.js
    Api Key updated

--- Premiere-Pixel/js/home.js
+++ Premiere-Pixel/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '6a2e7716c72cbadc002f540f618ee53e';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
